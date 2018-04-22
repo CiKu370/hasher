a4f58d4c613e1590394a8b4c068472792c84a829
CiKu370 <37820648+CiKu370@users.noreply.github.com>
2018-04-23 00:30:49 +0700

    Add files via upload

--- hash.py
+++ hash.py
@@ -56,19 +56,8 @@ elif sys.platform == "win32":
 	C = ''  # Cyan
 	P = ''  # Random color
 
-def banner_old():
-
-	print (Y+"\n 0{==================================================}0")
-	print (Y+" | "+P+"   __ __         __            "+R+"Hash Cracker "+W+"2.3.6"+Y+"  |")
-	print (Y+" | "+P+"  / // /__  ___ / /  ___ ____  "+Y+"                    |")
-	print (Y+" | "+P+" / _  / _ `(_-</ _ \/ -_) __/  "+B+"["+W+"="+B+"]"+W+" Author : Ci Ku"+Y+"  |")
-	print (Y+" | "+P+"/_//_/\_,_/___/_//_/\__/_/     "+B+"["+W+"="+B+"] "+W+"Team   : ??"+Y+"     |")
-	print (Y+" |                                                    |")
-	print (Y+" |        "+B+"  ["+R+"+"+B+"] "+W+"python2 "+sys.argv[0]+" --info "+B+"["+R+"+"+B+"]"+Y+"            |")
-	print (Y+" 0{==================================================}0\n")
-
 def banner():
-	print (RR+'\n              Hash Cracker'+WW+' 2.3.6')
+	print (RR+'\n              Hash Cracker'+WW+' 3.0.1')
 	print (W+'  -------------------------------------------')
 	print (P+'  88  88    db    .dP"Y8 88  88 88888 88""Yb ')
 	print (P+'  88  88   dPYb   `Ybo." 88  88 88__  88__dP ')
@@ -82,12 +71,12 @@ def info():
 	print (Y+"\n 0{======================"+W+" INFO "+Y+"=======================}0")
 	print (Y+" |"+B+" ["+R+"="+B+"] "+W+"Name     "+C+":"+W+" Hasher"+Y+"                               |")
 	print (Y+" |"+B+" ["+R+"="+B+"] "+W+"Code     "+C+":"+W+" python"+Y+"                               |")
-	print (Y+" |"+B+" ["+R+"="+B+"] "+W+"Version  "+C+":"+W+" 2.3.6 (beta)"+Y+"                         |")
-	print (Y+" |"+B+" ["+R+"="+B+"] "+W+"Author   "+C+":"+W+" debby anggraini ~ Ci Ku"+Y+"              |")
+	print (Y+" |"+B+" ["+R+"="+B+"] "+W+"Version  "+C+":"+W+" 3.0.1 (alpha)"+Y+"                        |")
+	print (Y+" |"+B+" ["+R+"="+B+"] "+W+"Author   "+C+":"+W+" Ci Ku "+R+"a.k.a "+W+"Xnver404"+Y+"                 |")
 	print (Y+" |"+B+" ["+R+"="+B+"] "+W+"Email    "+C+":"+W+" XnVer404@gmail.com"+Y+"                   |")
 	print (Y+" |"+B+" ["+R+"="+B+"] "+W+"Github   "+C+":"+W+" https://github.com/ciku370"+Y+"           |")
 	print (Y+" |"+B+" ["+R+"="+B+"] "+W+"Date     "+C+":"+W+" 23 - 02 - 2018"+Y+"                       |")
-	print (Y+" |"+B+" ["+R+"="+B+"] "+W+"Team     "+C+":"+W+" ??"+Y+"                                   |")
+	print (Y+" |"+B+" ["+R+"="+B+"] "+W+"Team     "+C+":"+W+" Blackhole Security"+Y+"                   |")
 	print (Y+" 0{===================================================}0\n")
 	print (B+" ["+R+"="+B+"] "+W+"python2 "+sys.argv[0]+" -u           to update wordlist "+B+"["+R+"="+B+"]")
 	print (B+"\n ["+R+"="+B+"] "+W+"list hash supported : "+Y+"["+W+"01"+Y+"] "+C+"md4")
@@ -98,7 +87,13 @@ def info():
 	print (Y+"                           ["+W+"06"+Y+"] "+C+"sha384")
 	print (Y+"                           ["+W+"07"+Y+"] "+C+"sha512")
 	print (Y+"                           ["+W+"08"+Y+"] "+C+"ripemd160")
-	print (Y+"                           ["+W+"09"+Y+"] "+C+"whirlpool\n"+W)
+	print (Y+"                           ["+W+"09"+Y+"] "+C+"whirlpool")
+	print (Y+"                           ["+W+"10"+Y+"] "+C+"MySQL 3.2.3")
+	print (Y+"                           ["+W+"11"+Y+"] "+C+"MySQL 4.1")
+	print (Y+"                           ["+W+"12"+Y+"] "+C+"MSSQL 2000")
+	print (Y+"                           ["+W+"13"+Y+"] "+C+"MSSQL 2005")
+	print (Y+"                           ["+W+"14"+Y+"] "+C+"Nthash")
+	print (Y+"                           ["+W+"15"+Y+"] "+C+"lmhash\n"+W)
 
 def Update():
 	banner()
@@ -125,13 +120,25 @@ def Update():
 
 
 try:
-        from tqdm import *
+
+	# module tambahan
+	import progressbar
+	from passlib.hash import mysql323 as m20
+	from passlib.hash import mysql41 as m25
+	from passlib.hash import mssql2000 as ms20
+	from passlib.hash import mssql2005 as ms25
+	from passlib.hash import nthash as nthash
+	from passlib.hash import lmhash as lmhash
+
 except ImportError:
         banner()
 	time.sleep(0.5)
-        print (B+"["+W+"="+B+"] "+G+"installing module "+R+"tqdm\n"+W)
+        print (B+"["+W+"="+B+"] "+G+"installing module "+R+"progressbar , passlib\n"+W)
+
 	os.system("pip2 install --upgrade pip")
-        os.system("pip2 install tqdm")
+	os.system("pip2 install passlib")
+	os.system("pip2 knstall progressbar")
+
 	print (B+"\n["+W+"="+B+"] "+G+"install success , run program again\n"+W)
         sys.exit()
 
@@ -139,7 +146,6 @@ def hash():
 	banner()
 
 	hash_str = raw_input(B+"["+W+"?"+B+"]"+G+" Hash : "+W)
-	hash_str = hash_str.lower()
 	time.sleep(0.5)
 	print (B+"["+R+"="+B+"] "+G+"Cek Hash Type ...")
 	time.sleep(1)
@@ -147,14 +153,35 @@ def hash():
 
 	# Contoh Hash nya , nb : jangan di ubah ntar error
 
-	SHA512='dd0ada8693250b31d9f44f3ec2d4a106003a6ce67eaa92e384b356d1b4ef6d66a818d47c1f3a2c6e8a9a9b9bdbd28d485e06161ccd0f528c8bbb5541c3fef36f'
-	md = 'ae11fd697ec92c7c98de3fac23aba525'
-	sha1 = '4a1d4dbc1e193ec3ab2e9213876ceb8f4db72333'
-	sha224 ='e301f414993d5ec2bd1d780688d37fe41512f8b57f6923d054ef8e59'
-	sha384 = '3b21c44f8d830fa55ee9328a7713c6aad548fe6d7a4a438723a0da67c48c485220081a2fbc3e8c17fd9bd65f8d4b4e6b'
-	sha256 = '2c740d20dab7f14ec30510a11f8fd78b82bc3a711abe8a993acdb323e78e6d5e'
+	SHA512= ('dd0ada8693250b31d9f44f3ec2d4a106003a6ce67eaa92e384b356d1b4ef6d66a818d47c1f3a2c6e8a9a9b9bdbd28d485e06161ccd0f528c8bbb5541c3fef36f')
+	md = ('ae11fd697ec92c7c98de3fac23aba525')
+	sha1 = ('4a1d4dbc1e193ec3ab2e9213876ceb8f4db72333')
+	sha224 = ('e301f414993d5ec2bd1d780688d37fe41512f8b57f6923d054ef8e59')
+	sha384 = ('3b21c44f8d830fa55ee9328a7713c6aad548fe6d7a4a438723a0da67c48c485220081a2fbc3e8c17fd9bd65f8d4b4e6b')
+	sha256 = ('2c740d20dab7f14ec30510a11f8fd78b82bc3a711abe8a993acdb323e78e6d5e')
+	mysql1323 = ("5d2e19393cc5ef67")
+	mysql41 = ("*88166B019A3144C579AC4A7131BCC3AD6FF61DA6")
+	mssql2000 = ("0x0100DE9B3306258B37432CAC3A6FB7C638946FA393E09C9CBC0FA8C6E03B803390B1C3E7FB112A21B2304595D490")
+	mssql2005 = ('0x01008110620C7BD03A38A28A3D1D032059AE9F2F94F3B74397F8')
 
-	if len(hash_str)==len(SHA512) and hash_str.isdigit()==False and hash_str.isalpha()==False and hash_str.isalnum()==True:
+	if len(hash_str)==len(mysql1323) and hash_str.isdigit()==False and hash_str.isalpha()==False and hash_str.isalnum()==True:
+		print (B+"["+R+"="+B+"] "+G+"hash type : "+W+"mysql 3.2.3")
+		hash = "mysql1323"
+
+	elif len(hash_str)==len(mysql41) and "*" in hash_str:
+		print (B+"["+R+"="+B+"] "+G+"hash type : "+W+"mysql 4.1")
+		hash = "mysql41"
+
+	elif len(hash_str)==len(mssql2000) and "0x0" in hash_str:
+		print (B+"["+R+"="+B+"] "+G+"hash type : "+W+"Mssql2000")
+		hash = "mssql2000"
+
+	elif len(hash_str)==len(mssql2005) and "0x0" in hash_str:
+		print (B+"["+R+"="+B+"] "+G+"hash type : "+W+"Mssql2005")
+                hash = "mssql2005"
+
+
+	elif len(hash_str)==len(SHA512) and hash_str.isdigit()==False and hash_str.isalpha()==False and hash_str.isalnum()==True:
 	        print (Y+"   ["+W+"01"+Y+"] "+C+"sha512")
 		print (Y+"   ["+W+"02"+Y+"] "+C+"whirlpool")
 		time.sleep(0.3)
@@ -173,6 +200,9 @@ def hash():
 
 		print (Y+"   ["+W+"01"+Y+"] "+C+"md4")
 		print (Y+"   ["+W+"02"+Y+"] "+C+"md5")
+		print (Y+"   ["+W+"03"+Y+"] "+C+"Nthash")
+		print (Y+"   ["+W+"04"+Y+"] "+C+"Lmhash")
+
 		time.sleep(0.3)
 		cek = raw_input(B+"["+W+"?"+B+"] "+G+"Choose Hash "+Y+">>> "+W)
 
@@ -227,6 +257,11 @@ def hash():
 				print (B+"["+W+"{}"+B+"]"+G+" Timeout\n").format(date)
 				hash = "md5"
 
+		elif cek == "03" or cek == "3" or cek.upper() == "NTHASH":
+			hash = "nthash"
+
+		elif cek == "04" or cek == "4" or cek.upper() == "LMHASH":
+			hash = "lmhash"
 
 		else:
 			print (R+"["+W+"!"+R+"] "+G+"Exiting ... \n"+W)
@@ -314,6 +349,7 @@ def hash():
 		except IOError:
 				print (B+"["+W+"{}"+B+"]"+G+" Timeout\n").format(date)
 				hash = "sha256"
+
 	else:
 		print (R+"["+W+"!"+R+"] "+G+"Hash error\n"+W)
 		sys.exit()
@@ -335,32 +371,135 @@ def hash():
 	print (B+"["+W+"*"+B+"] "+G+"start ..\n")
 	time.sleep(1)
 	print (B+"["+W+"{}"+B+"] "+G+"Cracking..."+Y).format(start)
-#	try:
-	for line in tqdm(w):
 
-		line = line.strip()
-	        h = hashlib.new(hash)
-		h.update(line)
+	pbar = progressbar.ProgressBar()
 
-		if "CiKu370" in line:
-			print (B+"["+W+"{}"+B+"]"+G+" password not found\n"+W).format(date)
-			sys.exit()
+	if hash == "mysql1323":
+		hash_str = hash_str.lower()
+		for line in pbar(w):
+			line = line.strip()
+			h = m20.encrypt(line)
 
-		if h.hexdigest() == hash_str:
-			end = time.asctime()
-			time.sleep(0.3)
-			print (B+"\n["+W+"{}"+B+"] "+G+"Stopped...\n").format(end)
-			time.sleep(0.3)
-			print (B+"["+W+"="+B+"]"+G+" password found ")
-               		print (B+"["+R+"+"+B+"] "+W+hash_str+Y+" 0={==> "+W+line+W)
-			sys.exit()
+			if h == hash_str:
 
-#	except UnboundLocalError:
-#		print (R+"\n["+W+"!"+R+"] "+G+"Error Hash Type Not Supported\n"+W)
-#		sys.exit()
-#	except IOError:
-#		print (R+"["+W+"!"+R+"]"+G+" I cannot load this hash :"+W+hash)
-#		sys.exit()
+				end = time.asctime()
+                                time.sleep(0.3)
+                                print (B+"\n["+W+"{}"+B+"] "+G+"Stopped...\n").format(end)
+                                time.sleep(0.3)
+                                print (B+"["+W+"="+B+"]"+G+" password found ")
+				print (B+"["+R+"+"+B+"] "+W+hash_str+Y+" 0={==> "+W+line+W+"\n")
+                                sys.exit()
+
+		print (B+"["+W+"{}"+B+"]"+G+" password not found\n"+W).format(date)
+		sys.exit()
+
+	elif hash == "lmhash":
+
+		hasb_str = hash_str.upper()
+		for line in pbar(w):
+			line = line.strip()
+			h = lmhash.encrypt(line)
+			if h == hash_str:
+				end = time.asctime()
+                                time.sleep(0.3)
+                                print (B+"\n["+W+"{}"+B+"] "+G+"Stopped...\n").format(end)
+                                time.sleep(0.3)
+                                print (B+"["+W+"="+B+"]"+G+" password found ")
+                                print (B+"["+R+"+"+B+"] "+W+hash_str+Y+" 0={==> "+W+line+W+"\n")
+                                sys.exit()
+
+		print (B+"["+W+"{}"+B+"]"+G+" password not found\n"+W).format(date)
+                sys.exit()
+
+	elif hash == "nthash":
+
+		hasb_str = hash_str.upper()
+                for line in pbar(w):
+                        line = line.strip()
+                        h = nthash.encrypt(line)
+
+                        if h == hash_str:
+                                end = time.asctime()
+                                time.sleep(0.3)
+                                print (B+"\n["+W+"{}"+B+"] "+G+"Stopped...\n").format(end)
+                                time.sleep(0.3)
+                                print (B+"["+W+"="+B+"]"+G+" password found ")
+                                print (B+"["+R+"+"+B+"] "+W+hash_str+Y+" 0={==> "+W+line+W+"\n")
+                                sys.exit()
+
+	elif hash == "mysql41":
+		hash_str = hash_str.upper()
+		for line in pbar(w):
+			line = line.strip()
+			h = m25.encrypt(line)
+
+			if h == hash_str:
+				end = time.asctime()
+                                time.sleep(0.3)
+                                print (B+"\n["+W+"{}"+B+"] "+G+"Stopped...\n").format(end)
+                                time.sleep(0.3)
+                                print (B+"["+W+"="+B+"]"+G+" password found ")
+                                print (B+"["+R+"+"+B+"] "+W+hash_str+Y+" 0={==> "+W+line+W+"\n")
+                                sys.exit()
+
+		print (B+"["+W+"{}"+B+"]"+G+" password not found\n"+W).format(date)
+                sys.exit()
+
+	elif hash == "mssql2000":
+		hash_str = hash_str.upper()
+		for line in pbar(w):
+			line = line.strip()
+			h = ms20.encrypt(line)
+
+			if h == hash_str:
+				end = time.asctime()
+                                time.sleep(0.3)
+                                print (B+"\n["+W+"{}"+B+"] "+G+"Stopped...\n").format(end)
+                                time.sleep(0.3)
+                                print (B+"["+W+"="+B+"]"+G+" password found ")
+                                print (B+"["+R+"+"+B+"] "+W+hash_str+Y+" 0={==> "+W+line+W+"\n")
+                                sys.exit()
+
+		print (B+"["+W+"{}"+B+"]"+G+" password not found\n"+W).format(date)
+                sys.exit()
+
+
+	elif hash == "mssql2005":
+		hasb_str = hash_str.upper()
+                for line in pbar(w):
+                        line = line.strip()
+                        h = ms25.encrypt(line)
+
+                        if h == hash_str:
+                                end = time.asctime()
+                                time.sleep(0.3)
+                                print (B+"\n["+W+"{}"+B+"] "+G+"Stopped...\n").format(end)
+                                time.sleep(0.3)
+                                print (B+"["+W+"="+B+"]"+G+" password found ")
+                                print (B+"["+R+"+"+B+"] "+W+hash_str+Y+" 0={==> "+W+line+W+"\n")
+                                sys.exit()
+
+                print (B+"["+W+"{}"+B+"]"+G+" password not found\n"+W).format(date)
+		sys.exit()
+	else:
+		hash_str = hash_str.lower()
+		for line in pbar(w):
+
+			line = line.strip()
+		        h = hashlib.new(hash)
+			h.update(line)
+
+			if h.hexdigest() == hash_str:
+				end = time.asctime()
+				time.sleep(0.3)
+				print (B+"\n["+W+"{}"+B+"] "+G+"Stopped...\n").format(end)
+				time.sleep(0.3)
+				print (B+"["+W+"="+B+"]"+G+" password found ")
+        	       		print (B+"["+R+"+"+B+"] "+W+hash_str+Y+" 0={==> "+W+line+W+"\n")
+				sys.exit()
+
+		print (B+"["+W+"{}"+B+"]"+G+" password not found\n"+W).format(date)
+		sys.exit()
 
 try:
 	if sys.argv[1] == "-u":
